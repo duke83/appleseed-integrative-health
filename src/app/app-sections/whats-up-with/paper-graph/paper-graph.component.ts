581069b43cf0ed7f32d5aa41c3085400858a8cfb
Kent Merrell <3596685+duke83@users.noreply.github.com>
2018-08-04 14:33:40 -0400

    Able to run and control paperjs within angular component

--- src/app/app-sections/whats-up-with/paper-graph/paper-graph.component.ts
+++ src/app/app-sections/whats-up-with/paper-graph/paper-graph.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { PaperScope, Project, Path, Point, Tool, View } from 'paper';
 
 @Component({
   selector: 'app-paper-graph',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaperGraphComponent implements OnInit {
 
+  @ViewChild('canvasElement') canvasElement: ElementRef;
+  scope: PaperScope;
+  project: Project;
+  yAxis: Path;
+
+
+
   constructor() { }
 
   ngOnInit() {
+    this.scope = new PaperScope();
+    this.project = new Project(this.canvasElement.nativeElement);
+    this.yAxis = new Path();
+    // ... add some points, set some properties
+    this.yAxis.strokeColor = 'black';
+    this.yAxis.add(new Point(10, 10));
+    this.yAxis.add(new Point(10, 300));
+    this.yAxis.add(new Point(300, 300));
+
+    this.project.activeLayer.addChild(this.yAxis);
   }
+
+  changeColor() {
+    this.yAxis.strokeColor = 'green';
+    this.yAxis.strokeWidth = 20;
+    this.scope.view.onFrame = (event) => {
+      console.log(event);
+     this.yAxis.rotate(3);
+    };
+  }
+
 
 }
