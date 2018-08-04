4e61fd60f938e785c721d3582f15d7d2c108d7a7
Kent Merrell <3596685+duke83@users.noreply.github.com>
2018-08-04 09:12:48 -0400

    Added paperjs to project. Updated npm.

---
+++ src/app/app-sections/whats-up-with/whats-up-with.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WhatsUpWithRoutingModule } from './whats-up-with-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    WhatsUpWithRoutingModule
+  ],
+  declarations: []
+})
+export class WhatsUpWithModule { }
